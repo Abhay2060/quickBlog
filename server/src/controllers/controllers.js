0453fee1c8d3e2dc062775dc8f8aea5f2c9f0013
Abhay Kumar <152151831+Abhay2060@users.noreply.github.com>
2026-02-21 12:16:26 +0530

    Update controllers.js

--- server/src/controllers/controllers.js
+++ server/src/controllers/controllers.js
@@ -67,8 +67,8 @@ export const adminLogin = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
